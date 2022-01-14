24a117d0bc0c66a696c9f64274675a336972370e
Francuziczek <92163538+Francuziczek@users.noreply.github.com>
2022-01-14 15:16:46 +0100

    Update Plik zawierajacy wszystkie polecenia pisane w putty.sql

--- Plik zawierajacy wszystkie polecenia pisane w putty.sql
+++ Plik zawierajacy wszystkie polecenia pisane w putty.sql
@@ -518,3 +518,20 @@ WHERE sz.nazwa_statusu_zamowienia
 
 
 #10.
+
+
+
+TRIGGERY
+1#
+DELIMITER //
+CREATE TRIGGER kreatura_before_insert
+BEFORE INSERT ON kreatura
+FOR EACH ROW
+BEGIN
+IF NEW.waga <= 0
+THEN
+SET NEW.waga = 1;
+END IF;
+END
+//
+DELIMITER ;
